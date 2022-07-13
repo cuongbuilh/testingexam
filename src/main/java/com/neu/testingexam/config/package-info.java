9f948be781a145b93d7f32e779ddbfed873ec739
CuongBui <cuongbuilh@gmail.com>
2022-07-13 14:23:40 +0700

    Initial version of tesingexam generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/neu/testingexam/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.neu.testingexam.config;
---
+++ src/main/java/com/neu/testingexam/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.neu.testingexam.repository;
+
+import com.neu.testingexam.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
