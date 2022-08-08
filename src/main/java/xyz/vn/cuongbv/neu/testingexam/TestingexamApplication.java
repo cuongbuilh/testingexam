057692e3f8ef656c06432b4eb55ad5bbf7357604
CuongBui <cuongbuilh@gmail.com>
2022-08-08 23:03:23 +0700

    init v2

---
+++ src/main/java/xyz/vn/cuongbv/neu/testingexam/TestingexamApplication.java
@@ -0,0 +1,13 @@
+package xyz.vn.cuongbv.neu.testingexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingexamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/vn/cuongbv/neu/testingexam/TestingexamApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.vn.cuongbv.neu.testingexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingexamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
